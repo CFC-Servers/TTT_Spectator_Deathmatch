e5b1aa714a7ff8210b35d766b34816051bb438a1
Pierce <pierce@vulpes.dev>
2023-10-20 18:27:36 -0400

    update weapon stats to more closely match base TTT guns

--- lua/weapons/weapon_ghost_sledge.lua
+++ lua/weapons/weapon_ghost_sledge.lua
@@ -1,11 +1,11 @@
 if SERVER then
-	AddCSLuaFile()
+    AddCSLuaFile()
 else
-	SWEP.PrintName = "H.U.G.E-249"
-	SWEP.Slot = 2
-	SWEP.Icon = "vgui/ttt/icon_m249"
-	SWEP.ViewModelFlip = false
-	SWEP.IconLetter = "z"
+    SWEP.PrintName = "H.U.G.E-249"
+    SWEP.Slot = 2
+    SWEP.Icon = "vgui/ttt/icon_m249"
+    SWEP.ViewModelFlip = false
+    SWEP.IconLetter = "z"
 end
 
 SWEP.HoldType = "crossbow"
@@ -19,21 +19,21 @@ SWEP.AutoSpawnable = false
 SWEP.Kind = WEAPON_HEAVY
 SWEP.WeaponID = AMMO_M249
 
-SWEP.Primary.Damage = 13
-SWEP.Primary.Delay = 0.05
-SWEP.Primary.Cone = 0.09
+SWEP.Primary.Damage = 8
+SWEP.Primary.Delay = 0.06
+SWEP.Primary.Cone = 0.065
 SWEP.Primary.ClipSize = 150
 SWEP.Primary.ClipMax = 150
-SWEP.Primary.DefaultClip = 300
+SWEP.Primary.DefaultClip = 1500
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "AirboatGun"
-SWEP.Primary.Recoil = 1.9
-SWEP.Primary.Sound = Sound("Weapon_m249.Single")
+SWEP.Primary.Recoil = 1.75
+SWEP.Primary.Sound = Sound( "Weapon_m249.Single" )
 SWEP.UseHands = true
 SWEP.ViewModel = "models/weapons/cstrike/c_mach_m249para.mdl"
 SWEP.WorldModel = "models/weapons/w_mach_m249para.mdl"
 
 SWEP.HeadshotMultiplier = 2.2
 
-SWEP.IronSightsPos = Vector(-5.96, -5.119, 2.349)
-SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.IronSightsPos = Vector( -5.96, -5.119, 2.349 )
+SWEP.IronSightsAng = Vector( 0, 0, 0 )
